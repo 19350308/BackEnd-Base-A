3981d380e029d0292c0ec6df5c5b99a428e35361
Luis Eduardo Miranda Mora <isc19350308@gmail.com>
2022-11-05 15:58:27 -0600

    Estados de Errores

--- routes/messager.js
+++ routes/messager.js
@@ -6,8 +6,8 @@ router.get('/', rootMessage) //end point
 router.get('/hi', hiMessage) //end point
 router.get('/bye', byeMessage)//end point
 
-router.get('/', postMessage) //end point
-router.get('/', putMessage) //end point
-router.get('/', deleteMessage)//end point
+router.post('/', postMessage) //end point
+router.put('/', putMessage) //end point
+router.delete('/', deleteMessage)//end point
 
 module.exports = router
