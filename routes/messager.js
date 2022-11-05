714fd5dbc911fbdb9686e8cb1a24f4bfb5a217ca
Luis Eduardo Miranda Mora <isc19350308@gmail.com>
2022-11-05 14:59:27 -0600

    Mensaje Json

--- routes/messager.js
+++ routes/messager.js
@@ -7,7 +7,7 @@ router.get('/hi', hiMessage) //end point
 router.get('/bye', byeMessage)//end point
 
 router.get('/', postMessage) //end point
-router.get('/hi', putMessage) //end point
-router.get('/bye', deleteMessage)//end point
+router.get('/', putMessage) //end point
+router.get('/', deleteMessage)//end point
 
 module.exports = router
