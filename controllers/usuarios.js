695a036f18114a510e301ccb1f3b35e6cec45434
Luis Eduardo Miranda Mora <isc19350308@gmail.com>
2022-11-05 20:28:56 -0600

    Contraseña Cifrada

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,5 @@
 const { request, response } = require("express");
+const bcryptjs = require("bcryptjs")
 const pool=require("../db/connection");
 
 //const { connect } = require("../routes/messager");
@@ -117,6 +118,9 @@ const getUsers = async (req=request, res= response)=>{
                 return
             }
 
+            const salt = bcryptjs.genSaltSync()
+            const ContrasenaCifrada = bcryptjs.hashSync(Contrasena, salt)
+
             const {affectedRows} =await conn.query(`
                 INSERT INTO usuarios(
                     Usuario, 
@@ -133,7 +137,7 @@ const getUsers = async (req=request, res= response)=>{
                     '${Apellidos}',
                     '${Edad}',
                     '${Genero || ' '}',
-                    '${Contrasena}', 
+                    '${ContrasenaCifrada}', 
                     '${Fecha_Nacimiento}',
                     '${Activo}'
                 )
